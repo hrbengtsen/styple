08cf0c0ccf3a61ffd293c2efa16d053169ac5b36
hrbengtsen <mikkelbengtsen@gmail.com>
2022-03-17 20:15:53 +0100

    test: add useDebounce.test.tsx

---
+++ packages/hooks/__tests__/useDebounce.test.tsx
@@ -0,0 +1,30 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useDebounce } from "..";
+
+describe("Test useDebounce", () => {
+  it("should work as expected", () => {
+    jest.useFakeTimers();
+
+    const callback = jest.fn();
+    const { result } = renderHook(() => useDebounce(callback, 0, 200));
+
+    // State should be set correctly and callback not called initially
+    expect(result.current[0]).toBe(0);
+    expect(callback).toBeCalledTimes(0);
+
+    act(() => {
+      // setState(5)
+      result.current[1](5);
+    });
+
+    expect(result.current[0]).toBe(5);
+
+    // Advance useDebounce timeout by 100ms, here callback should not have been called yet
+    jest.advanceTimersByTime(100);
+    expect(callback).toBeCalledTimes(0);
+
+    // 200ms advanced which match the delay, as such callback should have been called once
+    jest.advanceTimersByTime(100);
+    expect(callback).toBeCalledTimes(1);
+  });
+});
