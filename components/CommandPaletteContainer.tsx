cf4f222a70d77c6a2cc4db6caeb127006bc0bc57
Mikkel Bengtsen <mikkelbengtsen@gmail.com>
2022-09-25 16:07:01 +0200

    fix hydration error

--- components/CommandPaletteContainer.tsx
+++ components/CommandPaletteContainer.tsx
@@ -2,53 +2,42 @@ import {
   Button,
   CommandPalette,
   DataObj,
-  Flex,
-} from "../packages/design-system";
-import { Search } from "lucide-react";
-import { useRouter } from "next/router";
-import { NavLinkItem } from "./NavLinkItem";
+  Flex
+} from '../packages/design-system';
+import { Search } from 'lucide-react';
+import { useRouter } from 'next/router';
+import { NavLinkItem } from './NavLinkItem';
 
-const dummyCategories = ["Final docs pages"];
+const dummyCategories = ['Final docs pages'];
 
 const dummyData: DataObj[] = [
   {
-    label: "Do custom action",
+    label: 'Do custom action',
     action: () => {
-      alert("Custom action");
-    },
+      alert('Custom action');
+    }
   },
   {
-    label: "Introduction",
-    slug: "/docs/overview/introduction",
-    icon: <Search />,
+    label: 'Introduction',
+    slug: '/docs/overview/introduction',
+    icon: <Search />
   },
   {
-    label: "Getting started",
-    slug: "/docs/overview/getting-started",
+    label: 'Getting started',
+    slug: '/docs/overview/getting-started'
   },
   {
-    label: "Toast",
-    slug: "/docs/components/toast",
-    category: dummyCategories[0],
-  },
+    label: 'Toast',
+    slug: '/docs/components/toast',
+    category: dummyCategories[0]
+  }
 ];
 
 // Boilerplate component of how to implement a CommandPalette
 export const CommandPaletteContainer = () => {
   const router = useRouter();
 
-  return (
-    <CommandPalette
-      data={dummyData}
-      categories={dummyCategories}
-      trigger={<Button>Open CommandPalette</Button>}
-      router={router}
-      customLink={NavLinkItem}
-      preview={(selectedItemValue, data) => (
-        <Flex>test preview for {selectedItemValue}</Flex>
-      )}
-    />
-  );
+  return <CommandPalette />;
 };
 
 /*
--- packages/design-system/components/CommandPalette.tsx
+++ packages/design-system/components/CommandPalette.tsx
@@ -1,322 +1,44 @@
-import { ReactElement, useEffect, useState } from "react";
-import { Combobox } from "@headlessui/react";
-import { Button, NavItem, TextField, Container, Separator, Flex } from "..";
-import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { StyledOverlay, StyledContent } from "./Dialog";
-import { Search } from "lucide-react";
-import { css, keyframes } from "../stitches.config";
+import { ReactElement, useEffect, useState } from 'react';
+import { Command } from 'cmdk';
+import { Button, NavItem, TextField, Container, Separator, Flex } from '..';
+import * as DialogPrimitive from '@radix-ui/react-dialog';
+import { StyledOverlay, StyledContent } from './Dialog';
+import { Search } from 'lucide-react';
+import { css, keyframes } from '../stitches.config';
 
-const useEscape = (onEscape: () => void) => {
-  useEffect(() => {
-    const handleEsc = (event: KeyboardEvent) => {
-      if (event.key === "Escape") onEscape();
-    };
-
-    window.addEventListener("keydown", handleEsc);
-    return () => {
-      window.removeEventListener("keydown", handleEsc);
-    };
-  });
-};
-
-const optionsStyles = css({
-  p: "0",
-  listStyleType: "none",
-  position: "relative",
-  overflowY: "auto",
-  maxHeight: "75vh",
-  flex: 1,
-});
-
-const optionStyles = css({
-  cursor: "pointer",
-  mx: "$lg",
-  my: "$xs",
-  "&:first-child": {
-    mt: "$lg",
-  },
-  "&:last-child": {
-    mb: "$lg",
-  },
-});
-
-const contentFadeIn = keyframes({
-  "0%": { opacity: 0, transform: "translate(-50%, -2%) scale(.9)" },
-  "100%": { opacity: 1, transform: "translate(-50%, 0) scale(1)" },
-});
-
-export type DataObj = {
-  label: string;
-  category?: string;
-  icon?: ReactElement<any, any>;
-} & (
-  | { slug: string; action?: never } // Required to have atleast one of these props
-  | { slug?: never; action: () => void }
-  | { slug: string; action: () => void }
-);
-
-type CommandPaletteProps = {
-  data: DataObj[];
-  categories?: string[];
-  filterData?: (query: string, data: DataObj[]) => DataObj[];
-  preview?: (selectedValue: string, data: DataObj[]) => JSX.Element;
-  trigger: JSX.Element;
-  displayOptionsOnEmpty?: boolean;
-  placeholder?: string;
-  router: any;
-  customLink?: any;
-};
-
-export function CommandPalette({
-  data,
-  categories,
-  filterData,
-  preview,
-  trigger,
-  displayOptionsOnEmpty,
-  placeholder,
-  router,
-  customLink,
-}: CommandPaletteProps) {
+export function CommandPalette() {
   const [open, setOpen] = useState(false);
-  const [selectedValue, setSelectedValue] = useState("");
-  const [query, setQuery] = useState("");
 
-  // Force close of Dialog on ESC
-  useEscape(() => setOpen(false));
+  useEffect(() => {
+    const keyDownHandler = (e: KeyboardEvent) => {
+      if (e.key === 'k' && e.metaKey) {
+        setOpen(!open);
+      }
+    };
 
-  //useShortcut() <-- allow shortcut prop to allow opening through typing e.g: cmd+k
-
-  const filteredData = filterData
-    ? // Custom filtering
-      filterData(query, data)
-    : // Default filtering of data with query
-      data.filter((item) =>
-        item.label.toLowerCase().includes(query.toLowerCase())
-      );
-
-  const firstCategoryItemEncounters = categories?.map((category) =>
-    filteredData.findIndex((item) => item.category === category)
-  );
-
-  const NavItemCustom = customLink ?? NavItem;
+    document.addEventListener('keydown', keyDownHandler);
+    return () => document.removeEventListener('keydown', keyDownHandler);
+  });
 
   return (
-    <DialogPrimitive.Root open={open} onOpenChange={setOpen}>
-      <DialogPrimitive.Trigger asChild>{trigger}</DialogPrimitive.Trigger>
-      <StyledOverlay>
-        <StyledContent
-          css={{
-            "@bp1": {
-              minWidth: "$4xl",
-              maxWidth: preview ? "800px" : undefined,
-              overflowY: "hidden",
-              top: "10%",
-              transform: "translate(-50%, 0)",
-              animation: `${contentFadeIn} .15s ease-out`,
-              "@motion": {
-                animation: "none",
-              },
-            },
-          }}
-        >
-          <Combobox
-            value={selectedValue}
-            onChange={(value) => {
-              setSelectedValue(value);
-              const selectedData = filteredData.find(
-                (item) => item.label === value
-              );
+    <Command.Dialog
+      open={open}
+      onOpenChange={setOpen}
+      label="Global Command Menu"
+    >
+      <Command.Input />
+      <Command.List>
+        <Command.Empty>No results found.</Command.Empty>
 
-              if (selectedData?.slug) {
-                router.push(selectedData.slug);
-                return;
-              }
-              selectedData?.action?.();
-            }}
-            nullable
-          >
-            {({ open, activeOption }) => (
-              <>
-                <Container css={{ position: "sticky", top: "0" }}>
-                  <Container
-                    css={{
-                      color: "$text200",
-                      position: "absolute",
-                      top: "0",
-                      left: "0",
-                      height: "100%",
-                      display: "flex",
-                      alignItems: "center",
-                      pl: "$lg",
-                      pr: "$sm",
-                    }}
-                  >
-                    <Search size="20" />
-                  </Container>
-                  <Combobox.Input
-                    as={TextField}
-                    variant="ghost"
-                    css={{
-                      px: "$3xl",
-                      height: "46px",
-                      "&:hover, &:focus": {
-                        boxShadow: "none",
-                      },
-                      bg: "$bg200E",
-                    }}
-                    placeholder={placeholder ?? "Search..."}
-                    onChange={(event) => setQuery(event.target.value)}
-                  />
-                </Container>
-                {(displayOptionsOnEmpty ? true : query && open) &&
-                  filteredData.length > 0 && (
-                    <>
-                      <Separator css={{ position: "fixed", top: "46px" }} />
-                      <Flex>
-                        <Combobox.Options
-                          static
-                          hold
-                          className={optionsStyles()}
-                        >
-                          {filteredData.map((item, index) => {
-                            // Potential JSX of disabled category Combobox option
-                            let categoryOption;
+        <Command.Group heading="Letters">
+          <Command.Item>a</Command.Item>
+          <Command.Item>b</Command.Item>
+          <Command.Separator />
+          <Command.Item>c</Command.Item>
+        </Command.Group>
 
-                            // Index of category which current item is first category-item of
-                            const categoryOfItem =
-                              firstCategoryItemEncounters?.findIndex(
-                                (categoryFirstItemIndex) =>
-                                  categoryFirstItemIndex === index
-                              );
-
-                            const categoryExistAndItemIsMember =
-                              typeof categoryOfItem !== "undefined" &&
-                              categories &&
-                              categoryOfItem !== -1;
-
-                            if (categoryExistAndItemIsMember) {
-                              categoryOption = (
-                                <Combobox.Option
-                                  key={categories[categoryOfItem]}
-                                  value={categories[categoryOfItem]}
-                                  disabled
-                                >
-                                  <Container
-                                    css={{
-                                      mx: "$lg",
-                                      px: "$md",
-                                      mt: "$xl",
-                                      mb: "$xs",
-                                      fontWeight: "$semibold",
-                                      color: "$text100",
-                                    }}
-                                  >
-                                    {categories[categoryOfItem]}
-                                  </Container>
-                                </Combobox.Option>
-                              );
-                            }
-
-                            return (
-                              <>
-                                {categoryOption}
-                                <Combobox.Option
-                                  key={item.label}
-                                  value={item.label}
-                                  className={optionStyles()}
-                                >
-                                  {({ active, selected }) => {
-                                    // Item has slug -> it should be rendered as anchor tag
-                                    if (item.slug) {
-                                      return (
-                                        <>
-                                          <NavItemCustom
-                                            href={item.slug}
-                                            onClick={
-                                              item.action
-                                                ? () => item.action()
-                                                : undefined
-                                            }
-                                            css={{
-                                              py: "$sm",
-                                              px: "$md",
-                                              borderRadius: "$xl",
-                                              bg: `${
-                                                active
-                                                  ? "$button200"
-                                                  : selected
-                                                  ? "$button300"
-                                                  : "transparent"
-                                              }`,
-                                              color: "$text200",
-
-                                              display: "flex",
-                                              gap: "$sm",
-                                            }}
-                                          >
-                                            {item.icon}
-                                            {item.label}
-                                          </NavItemCustom>
-                                        </>
-                                      );
-                                    }
-
-                                    // Item not slug -> it should be rendered as button tag
-                                    return (
-                                      <>
-                                        {item.icon}
-                                        <Button
-                                          onClick={
-                                            item.action
-                                              ? () => item.action()
-                                              : undefined
-                                          }
-                                          css={{
-                                            width: "100%",
-                                            textAlign: "left",
-                                            py: "$sm",
-                                            px: "$md",
-                                            borderRadius: "$xl",
-                                            bg: `${
-                                              active
-                                                ? "$button200"
-                                                : selected
-                                                ? "$button300"
-                                                : "transparent"
-                                            }`,
-                                            color: "$text200",
-                                          }}
-                                        >
-                                          {item.label}
-                                        </Button>
-                                      </>
-                                    );
-                                  }}
-                                </Combobox.Option>
-                              </>
-                            );
-                          })}
-                        </Combobox.Options>
-                        <Container
-                          css={{
-                            display: "none",
-                            "@bp2": {
-                              display: "block",
-                              flex: 2,
-                            },
-                          }}
-                        >
-                          {activeOption && preview?.(activeOption, data)}
-                        </Container>
-                      </Flex>
-                    </>
-                  )}
-              </>
-            )}
-          </Combobox>
-        </StyledContent>
-      </StyledOverlay>
-    </DialogPrimitive.Root>
+        <Command.Item>Apple</Command.Item>
+      </Command.List>
+    </Command.Dialog>
   );
 }
